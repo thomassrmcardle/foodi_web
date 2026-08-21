021bd0075346c02cf88cbb2a6ecb751915b04a1c
Thomas <thomas_mcardle@outlook.com>
2026-08-22 00:20:40 +0100

    Fixed issue with share links.

--- app/share/[recipe_id]/page.tsx
+++ app/share/[recipe_id]/page.tsx
@@ -1,4 +1,4 @@
-
+import Head from 'next/head'
 import { useParams } from 'next/navigation'
 
 export default function Page() {
@@ -6,6 +6,9 @@ export default function Page() {
 
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <Head>
+                <title>{params.recipe_id}</title>
+            </Head>
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <h1 className="text-3xl">Shared Recipe</h1>
                 <p>This page should later be populated with content relevant to the shared recipe.</p>
