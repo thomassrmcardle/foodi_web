e83dcf8ac1ef10749f71126b7f92bf8cecaa682f
Thomas <thomas_mcardle@outlook.com>
2026-08-24 01:55:27 +0100

    Fixed alignment of items on home screen.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   }
 
   function DownloadLinks() {
-    return <div>
+    return <div className="flex flex-col mb-12 w-full items-center" style={{paddingTop: 16, paddingBottom: 4}}>
       <h2>Download Now</h2>
       <p>[Download links will go here...]</p>
     </div>
@@ -21,32 +21,32 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-4xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="w-full">
-          <h1 className="text-3xl text-centre">Cooking made simple.</h1>
-          <p className="text-centre">Epic sad</p>
+      <main className="flex flex-1 w-full max-w-4xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-center">
+        <div className="flex flex-col w-full items-center sm:items-center">
+          <h1 className="text-3xl text-centre">Recipes made simple.</h1>
+          <p className="text-centre">Making food has never been easier</p>
         </div>
 
         <DownloadLinks />
 
         <div className="max-w-3xl">
-          <div className="mb-12 flex flex-row flex-wrap-row gap-12">
+          <div className="mb-12 flex flex-row flex-wrap-row gap-16">
             <Placeholder />
-            <div className="flex flex-col flex-1">
+            <div className="justify-center flex flex-col flex-1">
               <h2>Cook</h2>
               <p>With a recipe system built around cooking</p>
             </div>
           </div>
-          <div className="mb-12 flex flex-row flex-wrap-row gap-12">
-            <div className="flex flex-col flex-1">
+          <div className="mb-12 flex flex-row flex-wrap-row gap-16">
+            <div className="justify-center flex flex-col flex-1">
               <h2>Discover</h2>
               <p>From a library of 100s of recipes</p>
             </div>
             <Placeholder />
           </div>
-          <div className="mb-12 flex flex-row flex-wrap-row gap-12">
+          <div className="mb-12 flex flex-row flex-wrap-row gap-16">
             <Placeholder />
-            <div className="flex flex-col flex-1">
+            <div className="justify-center flex flex-col flex-1">
               <h2>Share</h2>
               <p>Your recipes with the world!</p>
             </div>
