6effdf176afd5f9d176aa74b37dd35350816fac6
Thomas <thomas_mcardle@outlook.com>
2026-09-08 22:45:13 +0100

    Fixed build error with privacy policy.

--- app/policies/privacy/page.tsx
+++ app/policies/privacy/page.tsx
@@ -1,20 +1,19 @@
-import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 
 import { Metadata } from 'next'; 
-
-const response = await fetch(
-  "https://raw.githubusercontent.com/thomassrmcardle/Cherub_Terms/main/data_terms/content-1.0.md"
-);
-
-const markdown = await response.text();
 
 export const metadata: Metadata = {
   title: 'Privacy Policy',
   description: "This document details the usage of user data on the Cherub platform. This includes what is collected, how it is used, and how you can have it modified or deleted.",
 };
 
-export default function Home() {
+export default async function Home() {
+
+  const response = await fetch(
+    "https://raw.githubusercontent.com/thomassrmcardle/Cherub_Terms/main/data_terms/content-1.0.md"
+  );
+  
+  const markdown = await response.text();
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
